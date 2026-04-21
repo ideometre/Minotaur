48f0ce3790734b408bfc5d9e540f947369cf2ff0
Maxime Hanicotte <maxime.hanicotte@gmail.com>
2026-04-21 21:44:49 +0200

    Move sprites in a header

---
+++ src/sprites.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <Arduboy2.h>
+
+extern const unsigned char PROGMEM mx_0[];
+extern const unsigned char PROGMEM mx_1[];
+extern const unsigned char PROGMEM mx_2[];
+extern const unsigned char PROGMEM mx_3[];
+extern const unsigned char PROGMEM mx_4[];
+extern const unsigned char PROGMEM mx_5[];
+extern const unsigned char PROGMEM mx_6[];
+extern const unsigned char PROGMEM qrcode[];
+extern const unsigned char PROGMEM select[];
+extern const unsigned char PROGMEM minfront[];
+extern const unsigned char PROGMEM minback[];
+extern const unsigned char PROGMEM minleft[];
+extern const unsigned char PROGMEM minright[];
+extern const unsigned char PROGMEM sword[];
+extern const unsigned char PROGMEM string[];
+extern const unsigned char PROGMEM key[];
+extern const unsigned char PROGMEM input[];
+extern const unsigned char PROGMEM output[];
+extern const unsigned char PROGMEM closed[];
+extern const unsigned char PROGMEM end[];
+extern const unsigned char PROGMEM angle[];
+extern const unsigned char PROGMEM dot[];
+extern const unsigned char PROGMEM wallt[];
+extern const unsigned char PROGMEM straight[];
+extern const unsigned char PROGMEM cross[];
+extern const unsigned char PROGMEM empty[];
+extern const unsigned char PROGMEM front[];
+extern const unsigned char PROGMEM right[];
+extern const unsigned char PROGMEM left[];
+extern const unsigned char PROGMEM back[];
+extern const unsigned char PROGMEM armed_front[];
+extern const unsigned char PROGMEM armed_left[];
+extern const unsigned char PROGMEM armed_right[];
+extern const unsigned char PROGMEM armed_back[];
